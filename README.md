68dc3dc7e0061465bc8a27b7e7b69ec97a4e1fbc
Parampreet Singh <rubercuber.30@gmail.com>
2025-05-31 14:39:50 +0530

    feat: Langchain Chatgroq LLM integration

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Nemo Guardrails Chatbot
+
+
+
+- max token doesn't support by ollama: https://github.com/langchain-ai/langchain/issues/14715
--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import os
-import requests
+from groq import Groq
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
+# from nemoguardrails import RailsConfig
+# from nemoguardrails.integrations.langchain.runnable_rails import RunnableRails
+
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
 
 CHATGROQ_API_KEY = os.getenv("CHATGROQ_API_KEY")
-API_URL = "https://..."
 
+# config = RailsConfig.from_path("./rails_config")
 
-def query_llm(prompt):
-    ...
+chatgroqllm = ChatGroq(
+    api_key=CHATGROQ_API_KEY,
+    model="llama-3.3-70b-versatile",
+)
 
+# guardrails = RunnableRails(config=config)
 
+prompt = ChatPromptTemplate.from_messages([
+    # ("system", "You are a helpful assistant."),
+    ("user", "{input}")
+])
+
+output_parser = StrOutputParser()
+
+chain = prompt | chatgroqllm | output_parser
+# chain_with_guardrails = guardrails | chain
+
+messages = [
+    ("system", "You are a helpful assistant."),
+    ("user", "?"),
+]
+
+input_ = {"input": "What is the capital of India?"}
+
+print(chain.invoke(input_))
+# print(chain_with_guardrails.invoke(input_))
